58b08589425001ecbf3a7f9da7efab852aaa805b
ryan wetmore <ryanwetmore1014@gmil.com>
2024-05-23 13:33:28 -0400

    completed TDD exercise 2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (word == null || word.Length == 0)
+            {
+                return false;
+            }
+
+            string wordLowered = word.ToLower();
+            string backwards = "";
+
+            for (int i = wordLowered.Length - 1; i >= 0; i--)
+            {
+                backwards += word[i];
+            }
+
+            return wordLowered == backwards;
+
+            //if (wordLowered == backwards)
+            //{
+            //    return true;
+
+            //}
+            //else
+            //{
+            //return false;
+            //}
+
+
+        }
+    }
+}
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmithTest.cs
@@ -0,0 +1,36 @@
+﻿using PalindromeExercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PalindromeExerciseTests
+{
+    public class WordSmithTest
+    {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Hannah", true)]
+        [InlineData("Kayak", true)]
+        [InlineData("Ryan", false)]
+        [InlineData("Audi", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            //Arrange
+            var ws = new WordSmith();
+
+
+            //Act
+            var actual = ws.IsAPalindrome(word);
+
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
